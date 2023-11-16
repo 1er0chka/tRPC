e2703993d8cb579565531c724536aa95fb237ac7
1er0chka <galaburda.lera1@gmail.com>
2023-11-16 17:40:25 +0300

    import client's project

---
+++ client/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['assets.coincap.io'],
+    },
+    // ... здесь могут быть другие глобальные настройки для вашего проекта
+};
+
+module.exports = nextConfig;
