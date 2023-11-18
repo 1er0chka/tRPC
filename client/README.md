9d10310317c8cc9f97be21d5b63d35f4f49ce36b
1er0chka <galaburda.lera1@gmail.com>
2023-11-18 18:07:26 +0300

    moved client and server into packages

--- client/README.md
+++
@@ -1,1 +0,0 @@
-Open [(netlify](https://starlit-cannoli-f23ffc.netlify.app/)) with your browser to see the result.
--- client/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app";
-import { CoinInfoProvider } from "@/app/provider/CoinInfoContext";
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <CoinInfoProvider>
-      <Component {...pageProps} />
-    </CoinInfoProvider>
-  );
-}
