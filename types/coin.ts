4484a93141c2a90757819e4d47b713d73ad4f631
1er0chka <galaburda.lera1@gmail.com>
2023-11-22 19:33:14 +0300

    configuration updated

--- types/coin.ts
+++ types/coin.ts
@@ -10,36 +10,9 @@ export interface Coin {
     marketCapUsd: string;
 }
 
-export const defaultCoin: Coin = {
-    changePercent24Hr: "",
-    id: "",
-    marketCapUsd: "",
-    maxSupply: "",
-    name: "",
-    priceUsd: "",
-    rank: "",
-    supply: "",
-    symbol: "",
-};
-
 export interface History {
     priceUsd: string;
     time: string;
     circulatingSupply: string;
     date: string;
 }
-
-export interface Portfolio {
-    id: string;
-    number: number;
-    oldPrice: number;
-}
-
-export interface PortfolioCoin extends Portfolio {
-  newPrice: number;
-  difference: number;
-  name: string;
-  symbol: string
-}
-
-export type Sort = "rank" | "priceUsd" | "changePercent24Hr" | "marketCapUsd";
