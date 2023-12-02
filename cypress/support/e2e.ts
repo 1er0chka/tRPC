36aa7f0fef6ed80588c0115badb6c234d6a6d719
1er0chka <galaburda.lera1@gmail.com>
2023-12-02 12:46:23 +0300

    deleted cypress (version conflict)

--- cypress/support/e2e.ts
+++
@@ -1,8 +0,0 @@
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
-addMatchImageSnapshotCommand();
-
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
