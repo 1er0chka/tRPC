eb791051bdc0d49ebef68732a3a8166b99de902f
1er0chka <galaburda.lera1@gmail.com>
2023-12-03 17:56:34 +0300

    added cypress

---
+++ cypress/e2e/DataLoadTest.cy.ts
@@ -0,0 +1,25 @@
+describe('Data Load Test', () => {
+    it('successfully loads and displays coins data', () => {
+        cy.visit('https://wondrous-donut-cfa916.netlify.app/');
+
+        cy.get('[data-test-id="header-coin-card"]').each((card) => {
+            const text = card.text();
+            console.log(text)
+            expect(text).not.to.match(/NaN|undefined|^0$/);
+        });
+
+
+        // Проверяем наличие данных в таблице
+        cy.get('table').find('tr').should('have.length.at.least', 1);
+        cy.get('table').find('td').each(($td) => {
+            const text = $td.text();
+            expect(text).not.to.match(/NaN|undefined|^0$/);
+        });
+
+        // Проверяем, что нумерация страниц отображается
+        cy.get('.pagination').should('contain.text', '1');
+
+        // Визуальная проверка главной страницы
+       // cy.matchImageSnapshot('homepage');
+    });
+});
