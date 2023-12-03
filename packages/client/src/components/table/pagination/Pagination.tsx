6df8bed9f6db85f98570b723ad60fa4f8a8b09e5
1er0chka <galaburda.lera1@gmail.com>
2023-12-03 20:00:39 +0300

    removed data-testid in pagination

--- packages/client/src/components/table/pagination/Pagination.tsx
+++ packages/client/src/components/table/pagination/Pagination.tsx
@@ -26,9 +26,9 @@ const Pagination: FunctionComponent<IPaginationProps> = ({
     }, [offset]);
 
     return (
-        <div className={styles.pagination} data-testid="coins-table-pagination">
+        <div className={styles.pagination}>
             <Button onClick={decreaseOffset} disabled={offset - 40 < 0} label={"<"} mode={"primary"}/>
-            <div>
+            <div data-testid="coins-table-pagination">
                 {offset} - {offset + 40 > itemsNumber ? itemsNumber : offset + 40}
             </div>
             <Button
