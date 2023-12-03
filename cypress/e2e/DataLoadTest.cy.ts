adccc565b46d1afdd4b3ccfb59842a2ae8731574
1er0chka <galaburda.lera1@gmail.com>
2023-12-03 18:34:40 +0300

    added checks for text data - added check for coin name, symbol, price in HeaderCoinCard component

--- cypress/e2e/DataLoadTest.cy.ts
+++ cypress/e2e/DataLoadTest.cy.ts
@@ -1,13 +1,31 @@
+const checkTextContent = (text: string) => {
+    expect(text).not.to.be.empty
+    expect(text).not.to.match(/NaN|undefined/)
+};
+
+const checkNumericContent = (text: string,  checkForZero = true) => {
+    expect(text).not.to.be.empty
+    expect(text).not.to.match(/NaN|undefined/)
+    if (checkForZero) {
+        expect(text).not.to.equal('$0')
+        expect(text).not.to.equal('$0.00')
+    }
+};
+
+const getText = (element: JQuery<HTMLElement>, testId: string) => {
+    return element.find(`[data-test-id="${testId}"]`).text().trim()
+}
+
+
 describe('Data Load Test', () => {
     it('successfully loads and displays coins data', () => {
-        cy.visit('https://wondrous-donut-cfa916.netlify.app/');
+        cy.visit('https://wondrous-donut-cfa916.netlify.app/')
 
         cy.get('[data-testid="header-coin-card"]').each((card) => {
-            const text = card.text();
-            console.log(text)
-            expect(text).not.to.match(/NaN|undefined|^0$/);
+            checkTextContent(getText(card, 'coin-name'))
+            checkTextContent(getText(card, 'coin-symbol'))
+            checkNumericContent(getText(card, 'coin-price'))
         });
-
 
         // Проверяем наличие данных в таблице
         cy.get('table').find('tr').should('have.length.at.least', 1);
