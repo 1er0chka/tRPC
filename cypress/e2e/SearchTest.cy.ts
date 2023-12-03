461c4a0f752ef8b6433492c0ac171bd9fa0965da
1er0chka <galaburda.lera1@gmail.com>
2023-12-03 21:18:11 +0300

    added test for search

--- cypress/e2e/SearchTest.cy.ts
+++ cypress/e2e/SearchTest.cy.ts
@@ -1,7 +1,22 @@
 describe('Search Functionality Test', () => {
     it('automatically inputs a value and searches', () => {
+        const searchRequest = 'bitcoin'
         cy.visit('https://wondrous-donut-cfa916.netlify.app/')
-        cy.get('[data-testid="search-input"]').type('Bitcoin');
-        cy.get('[data-testid="search-button"]').click();
+
+        cy.get('[data-testid="coins-table-row"]').should('have.length.at.least', 1)
+        cy.get('[data-testid="search-input"]').type(searchRequest)
+
+        cy.intercept('POST', 'https://trpc-server-38lb.onrender.com/coincap/coins.getSearchResult').as('searchResults');
+        cy.get('[data-testid="search-button"]').click()
+        cy.wait('@searchResults');
+
+        cy.checkCoinsTable()
+
+        cy.get('[data-testid="coins-table"]').find('[data-testid="coins-table-row"]').each((tr) => {
+            const re = new RegExp(searchRequest, 'i'); // 'i' означает нечувствительность к регистру
+            expect(tr.find(`[data-testid="coin-name"]`).text().trim()).to.match(re);
+
+       //     expect(tr.find(`[data-testid="coin-name"]`).text().trim()).to.equal(searchRequest)
+        });
     });
 });
