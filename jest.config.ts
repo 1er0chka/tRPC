dcfb21f57c955bb7483bf7f79ff0ff9c8921b76c
1er0chka <galaburda.lera1@gmail.com>
2023-12-14 15:33:56 +0300

    added jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": 'ts-jest',
+        // process `*.tsx` files with `ts-jest`
+    },
+    rootDir: 'packages/',
+    moduleNameMapper: {
+        "\\.(css|less|scss|sss|styl)$": "identity-obj-proxy"
+    },
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    testResultsProcessor: "../node_modules/jest-html-reporter"
+
+};
